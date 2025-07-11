7a6d90186a72c1cdef83f250ab911d70f5cdd255
Zeeta:) <189728939+zetFF@users.noreply.github.com>
2025-07-12 02:00:34 +0700

    update

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -13,32 +13,32 @@ export const DATA = {
     "I'm a website developer and user interface designer. Currently, I work as a self-employed frontend developer, specializing in creating impressive websites that boost business. am a Web Developer who focuses on Frontend and UI UX Designer. I learned about Frontend art world since 2022 until now. I am able to use JavaScript Framework technology and UI Library.",
   avatarUrl: "/zet.png",
 
-gallery: [
-  {
-    href: "/blurry-bass.jpg",
-    image: "/blurry-bass.jpg",
-  },
-  {
-    href: "/red-tshirt.jpg",
-    image: "/red-tshirt.jpg",
-  },
-  {
-    href: "/image.png",
-    image: "/image.png",
-  },
-  {
-    href: "/smoke-shadow.jpg",
-    image: "/smoke-shadow.jpg",
-  },
-  {
-    href: "/zet.jpg",
-    image: "/zet.jpg",
-  },
-  {
-    href: "/graduate.jpg",
-    image: "/graduate.jpg",
-  },
-],
+  gallery: [
+    {
+      href: "/blurry-bass.jpg",
+      image: "/blurry-bass.jpg",
+    },
+    {
+      href: "/red-tshirt.jpg",
+      image: "/red-tshirt.jpg",
+    },
+    {
+      href: "/image.png",
+      image: "/image.png",
+    },
+    {
+      href: "/smoke-shadow.jpg",
+      image: "/smoke-shadow.jpg",
+    },
+    {
+      href: "/zet.jpg",
+      image: "/zet.jpg",
+    },
+    {
+      href: "/graduate.jpg",
+      image: "/graduate.jpg",
+    },
+  ],
 
   skills: [
     "React",
@@ -49,6 +49,7 @@ gallery: [
     "CSS",
     "Laravel",
     "Figma",
+     "Tailwind CSS",
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
@@ -143,164 +144,156 @@ gallery: [
     },
   ],
   projects: [
-    {
-      title: "Re4ctx Todos",
-      href: "https://github.com/zetFF/re4ctx-todo.git",
-      dates: "Mar 20, 2025 - Active",
+     {
+      title: "Tokopedia Clone",
+      href: "",
+      dates: "July 2025 - Active",
       active: true,
       description:
-        "this is a repository of Website-based TodoList Applications with Laravel Breeze Inertia React Technology Support. with UI integration from Shadcn UI. this application was developed to support the advancement of School graduation Proposals and Graduation Projects.",
-      technologies: [
-        "Laravel",
-        "React",
-        "Javascript",
-        "Inertia",
-        "PHP",
-        "MySQL",
-        "Shadcn UI",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://github.com/zetFF/all-stack-admin.git",
-          icon: <Icons.globe className="size-3" />,
-        },
-      ],
-      image: "/re4ctx.png",
-    },
-    {
-      title: "Todo-Board",
-      href: "https://github.com/zetFF/todo-board.git",
-      dates: "Feb 27, 2025 - Active",
-      active: true,
-      description:
-        "This repository is a TodoList Website Application. this application was created with the aim of being a requirement for graduation from my school. This application was developed with Laravel Blade Technology",
+        "tokopedia Clone is a website that is designed to almost match the structure and design concept of the Tokopedia website using Next JS technology.",
       technologies: [
         "Next.js",
         "Typescript",
-        "PostgreSQL",
-        "Prisma",
         "TailwindCSS",
-        "Stripe",
         "Shadcn UI",
-        "Magic UI",
+        "Clerk Auth",
       ],
       links: [
         {
           type: "Website",
-          href: "https://github.com/zetFF/todo-board.git",
+          href: "",
           icon: <Icons.globe className="size-3" />,
         },
-        {
-          type: "Source",
-          href: "https://github.com/zetFF/todo-board.git",
-          icon: <Icons.github className="size-3" />,
-        },
       ],
-      image: "/TodoSpace.png",
+      image: "/tokped.png",
     },
+    // {
+    //   title: "Re4ctx Todos",
+    //   href: "https://github.com/zetFF/re4ctx-todo.git",
+    //   dates: "Mar 20, 2025 - Active",
+    //   active: true,
+    //   description:
+    //     "this is a repository of Website-based TodoList Applications with Laravel Breeze Inertia React Technology Support. with UI integration from Shadcn UI. this application was developed to support the advancement of School graduation Proposals and Graduation Projects.",
+    //   technologies: [
+    //     "Laravel",
+    //     "React",
+    //     "Javascript",
+    //     "Inertia",
+    //     "PHP",
+    //     "MySQL",
+    //     "Shadcn UI",
+    //   ],
+    //   links: [
+    //     {
+    //       type: "Website",
+    //       href: "https://github.com/zetFF/all-stack-admin.git",
+    //       icon: <Icons.globe className="size-3" />,
+    //     },
+    //   ],
+    //   image: "/re4ctx.png",
+    // },
 
-    {
-      title: "Mockup KIP KAPUAS",
-      href: "https://www.figma.com/design/vZ72tHWCWVVidZfJdqcjhv/KIP-KAPUAS?t=ByoUDeIgyDQPsbjj-1",
-      dates: "July 2024, - Active",
-      active: true,
-      description:
-        "KIP KAPUAS is a mobile application designed to provide information on the Indonesia, Kapuas region with various forms of information, ranging from Radio, News, and other information.",
-      technologies: ["Figma"],
-      links: [
-        {
-          type: "Mockup",
-          href: "https://www.figma.com/design/vZ72tHWCWVVidZfJdqcjhv/KIP-KAPUAS?t=ByoUDeIgyDQPsbjj-1",
-          icon: <Icons.globe className="size-3" />,
-        },
-      ],
-      image: "/kip-kapuas.png",
-    },
-    {
-      title: "Mockup SIPD edb_gis",
-      href: "https://www.figma.com/design/fwt9dcvRqhxy4mN3n39vO9/Mockup-design-SIPD-ebd_gis---prototype?t=ByoUDeIgyDQPsbjj-1",
-      dates: "Juny 2024, - Active",
-      active: true,
-      description:
-        "SIPD edb_gis is a Dashboard designed to analyze the results of the Ministry of Home Affairs data. This project is a Mockup design with a new Design.",
-      technologies: ["Figma"],
-      links: [
-        {
-          type: "Mockup",
-          href: "https://www.figma.com/design/vZ72tHWCWVVidZfJdqcjhv/KIP-KAPUAS?t=ByoUDeIgyDQPsbjj-1",
-          icon: <Icons.globe className="size-3" />,
-        },
-      ],
-      image: "/sipd.png",
-    },
-    {
-      title: "System Development NUDP",
-      href: "https://www.figma.com/design/MovVbgFgboQkRXVwk0Ax2s/Mockup-Sysdev?node-id=1401-14131&t=spu2WCdiCtpMHe7v-1",
-      dates: "Mar 24, 2025 - Active",
-      active: true,
-      description:
-        "NUDP stands for National Urban Development Project or in Indonesian it is called the National Urban Development Project. This project was developed by the Indonesian Government in collaboration with the World Bank. NUDP has been implemented since 2020 and will be completed in December 2024 (duration around 5 years).",
-      technologies: ["Mockup"],
-      links: [
-        {
-          type: "Mockup",
-          href: "https://www.figma.com/design/MovVbgFgboQkRXVwk0Ax2s/Mockup-Sysdev?node-id=1401-14131&t=spu2WCdiCtpMHe7v-1",
-          icon: <Icons.globe className="size-3" />,
-        },
-      ],
-      image: "/sysdev.png",
-    },
-    {
-      title: "Wedding Website",
-      href: "https://github.com/zetFF/WeddWeb.git",
-      dates: "February 2025 - Active",
-      active: true,
-      description:
-        "A Wedding Website is a wedding invitation in the form of a website, which is designed from the perspective of the guest and the website owner.",
-      technologies: [
-         "Laravel",
-        "React",
-        "JavaScript",
-        "Inertia",
-        "PHP",
-        "MySQL",
-        "Shadcn UI",
-         "Filament",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://github.com/zetFF/WeddWeb.git",
-          icon: <Icons.globe className="size-3" />,
-        },
-      ],
-      image: "/wedding.png",
-    },
-    {
-      title: "Foodly-Marketplace",
-      href: "https://github.com/zetFF/foodly-marketplace.git",
-      dates: "Mar 24, 2025 - Active",
-      active: true,
-      description:
-        "A food marketplce website application designed for visual frontend and design. This application is not integrated with any database. The technology that supports this application is React Vite typscript",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "TailwindCSS",
-        "Shadcn UI",
-        "Lovable",
-        "vite",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://github.com/zetFF/foodly-marketplace.git",
-          icon: <Icons.globe className="size-3" />,
-        },
-      ],
-      image: "/Foodly.png",
-    },
+    // {
+    //   title: "Mockup KIP KAPUAS",
+    //   href: "https://www.figma.com/design/vZ72tHWCWVVidZfJdqcjhv/KIP-KAPUAS?t=ByoUDeIgyDQPsbjj-1",
+    //   dates: "July 2024, - Active",
+    //   active: true,
+    //   description:
+    //     "KIP KAPUAS is a mobile application designed to provide information on the Indonesia, Kapuas region with various forms of information, ranging from Radio, News, and other information.",
+    //   technologies: ["Figma"],
+    //   links: [
+    //     {
+    //       type: "Mockup",
+    //       href: "https://www.figma.com/design/vZ72tHWCWVVidZfJdqcjhv/KIP-KAPUAS?t=ByoUDeIgyDQPsbjj-1",
+    //       icon: <Icons.globe className="size-3" />,
+    //     },
+    //   ],
+    //   image: "/kip-kapuas.png",
+    // },
+    // {
+    //   title: "Mockup SIPD edb_gis",
+    //   href: "https://www.figma.com/design/fwt9dcvRqhxy4mN3n39vO9/Mockup-design-SIPD-ebd_gis---prototype?t=ByoUDeIgyDQPsbjj-1",
+    //   dates: "Juny 2024, - Active",
+    //   active: true,
+    //   description:
+    //     "SIPD edb_gis is a Dashboard designed to analyze the results of the Ministry of Home Affairs data. This project is a Mockup design with a new Design.",
+    //   technologies: ["Figma"],
+    //   links: [
+    //     {
+    //       type: "Mockup",
+    //       href: "https://www.figma.com/design/vZ72tHWCWVVidZfJdqcjhv/KIP-KAPUAS?t=ByoUDeIgyDQPsbjj-1",
+    //       icon: <Icons.globe className="size-3" />,
+    //     },
+    //   ],
+    //   image: "/sipd.png",
+    // },
+    // {
+    //   title: "System Development NUDP",
+    //   href: "https://www.figma.com/design/MovVbgFgboQkRXVwk0Ax2s/Mockup-Sysdev?node-id=1401-14131&t=spu2WCdiCtpMHe7v-1",
+    //   dates: "Mar 24, 2025 - Active",
+    //   active: true,
+    //   description:
+    //     "NUDP stands for National Urban Development Project or in Indonesian it is called the National Urban Development Project. This project was developed by the Indonesian Government in collaboration with the World Bank.",
+    //   technologies: ["Mockup"],
+    //   links: [
+    //     {
+    //       type: "Mockup",
+    //       href: "https://www.figma.com/design/MovVbgFgboQkRXVwk0Ax2s/Mockup-Sysdev?node-id=1401-14131&t=spu2WCdiCtpMHe7v-1",
+    //       icon: <Icons.globe className="size-3" />,
+    //     },
+    //   ],
+    //   image: "/sysdev.png",
+    // },
+    // {
+    //   title: "Wedding Website",
+    //   href: "https://github.com/zetFF/WeddWeb.git",
+    //   dates: "February 2025 - Active",
+    //   active: true,
+    //   description:
+    //     "A Wedding Website is a wedding invitation in the form of a website, which is designed from the perspective of the guest and the website owner.",
+    //   technologies: [
+    //     "Laravel",
+    //     "React",
+    //     "JavaScript",
+    //     "Inertia",
+    //     "PHP",
+    //     "MySQL",
+    //     "Shadcn UI",
+    //     "Filament",
+    //   ],
+    //   links: [
+    //     {
+    //       type: "Website",
+    //       href: "https://github.com/zetFF/WeddWeb.git",
+    //       icon: <Icons.globe className="size-3" />,
+    //     },
+    //   ],
+    //   image: "/wedding.png",
+    // },
+    // {
+    //   title: "Foodly Marketplace",
+    //   href: "https://github.com/zetFF/foodly-marketplace.git",
+    //   dates: "Mar 24, 2025 - Active",
+    //   active: true,
+    //   description:
+    //     "A food marketplce website application designed for visual frontend and design. This application is not integrated with any database. The technology that supports this application is React Vite typscript",
+    //   technologies: [
+    //     "Next.js",
+    //     "Typescript",
+    //     "TailwindCSS",
+    //     "Shadcn UI",
+    //     "Lovable",
+    //     "vite",
+    //   ],
+    //   links: [
+    //     {
+    //       type: "Website",
+    //       href: "https://github.com/zetFF/foodly-marketplace.git",
+    //       icon: <Icons.globe className="size-3" />,
+    //     },
+    //   ],
+    //   image: "/Foodly.png",
+    // },
   ],
 
   hackathons: [
